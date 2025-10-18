735dfdf3e02c5915541052f95d90f5f17615f5b9
GabrielPKTN <gabriellacerda20059@outlook.com>
2025-10-18 18:32:35 -0300

    feat(app): Adicionando a classificação indicativa do filme pelo JavaScript.

--- app.js
+++ app.js
@@ -16,16 +16,16 @@ async function retornaDadosFilmes() {
 
     const json = await jsonFilmes()
 
-    json.forEach(manipulaDadosFilme);
+    json.forEach(manipulaDadosFilme)
 }
 
-function manipulaDadosFilme(json) {
+async function manipulaDadosFilme(json) {
 
     const sliderItems = document.getElementById('container-items')
 
     const divBackgroundFilme = document.createElement("div")
     const imgBackgroundFilme = document.createElement("img")
-    
+
     const divContainerInfoFilmeSlider = document.createElement("div")
     const divInfoFilme = document.createElement("div")
     const divSinopseClassificacao = document.createElement("div")
@@ -34,6 +34,8 @@ function manipulaDadosFilme(json) {
     const sinopse = document.createElement("p")
     const divClassificacaoFilmeSlide = document.createElement("div")
     const divClassificacao = document.createElement("div")
+    const divClassificacaoIndicativa = document.createElement("div")
+    const imgClassificacaoIndicativa = document.createElement("img")
 
     divBackgroundFilme.classList.add("item")
 
@@ -44,14 +46,15 @@ function manipulaDadosFilme(json) {
     sinopse.classList.add("sinopse-filme-slideshow")
     divClassificacaoFilmeSlide.classList.add("classificacao-filme-slideshow")
     divClassificacao.classList.add("classificacao-slideshow")
+    divClassificacaoIndicativa.classList.add("classificacao-indicativa-slideshow")
 
     let imgBackground = json.backdrop_path
-    imgBackgroundFilme.src =  `https://image.tmdb.org/t/p/w500/${imgBackground}`
+    imgBackgroundFilme.src = `https://image.tmdb.org/t/p/w500/${imgBackground}`
 
     let posterFilm = json.poster_path
     imgInfoSlide.src = `https://image.tmdb.org/t/p/w200/${posterFilm}`
 
-    sinopse.textContent = json.overview 
+    sinopse.textContent = json.overview
 
     const limiteCaracteresSinopse = 150
 
@@ -90,32 +93,115 @@ function manipulaDadosFilme(json) {
         diretamente, tinha problemas com promisses, tentei criar uma cópia
         com json.parse, e stringify mas ainda sim não tive êxito, somente
         quando fiz com que uma variavel recebesse o valor de classificacaoFilme.
-        */ 
-       
+        */
+
         let copyClassificacaoFilme = classificacaoFilme
 
         while (copyClassificacaoFilme < classificacaoMaxima) {
-            
+
             const estrelaVazia = document.createElement("i")
             estrelaVazia.classList.add('fa-solid')
             estrelaVazia.classList.add('fa-star')
             estrelaVazia.classList.add('estrelaVazia')
-            
+
             divClassificacao.appendChild(estrelaVazia)
             copyClassificacaoFilme++
 
         }
 
-    } 
+        const spanClassificacao = document.createElement("span")
+        spanClassificacao.textContent = `(${json.vote_average.toFixed(1)})`
+        divClassificacao.appendChild(spanClassificacao)
+    }
     defineClassificacaoFilme()
 
+    divClassificacaoFilmeSlide.appendChild(divClassificacaoIndicativa)
+
+    switch (await retornaDadosClassificacaoFilmes(json)) {
+        case "L":
+            imgClassificacaoIndicativa.src = "./assets/Livre.png"
+            break;
+
+        case "6":
+            imgClassificacaoIndicativa.src = "./assets/6.png"
+            break;
+
+        case "12":
+            imgClassificacaoIndicativa.src = "./assets/12.png"
+            break;
+
+        case "14":
+            imgClassificacaoIndicativa.src = "./assets/14.png"
+            break;
+
+        case "16":
+            imgClassificacaoIndicativa.src = "./assets/16.png"
+            break;
+
+        case "18":
+            imgClassificacaoIndicativa.src = "./assets/18.png"
+            break;
+
+        default:
+            imgClassificacaoIndicativa.src = "./assets/default.png"
+            break;
+    }
+
+    divClassificacaoIndicativa.appendChild(imgClassificacaoIndicativa)
     divBackgroundFilme.appendChild(imgBackgroundFilme)
-    
+
 }
 
 // **************************************
 
-// **************************************lançamentos
+// **************************************Função para carregar a classificação indicativa do filme
+
+async function jsonClassificacaoFilmes(json) {
+    
+    const url = `https://api.themoviedb.org/3/movie/${json.id}/release_dates?api_key=${apiKey}`
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data.results
+}
+
+async function retornaDadosClassificacaoFilmes(jsonObject) {
+
+    const json = await jsonClassificacaoFilmes(jsonObject)
+
+    let result = manipulaDadosJsonClassificacao(json)
+
+    return result
+}
+
+function manipulaDadosJsonClassificacao(json) {
+    for (let classificacao of json) {
+
+        if (classificacao.iso_3166_1 == 'BR') {
+            
+            let classificacaoBr
+
+            classificacao.release_dates.forEach((object) => {
+                
+                /*
+                O retorno não foi colocado diretamente dentro do forEach
+                por conta que ele retorna referenciando o callback do objeto,
+                sendo assim a função que retorna os dados para a aplicação
+                sempre retornaria Undefined.
+                */
+
+                classificacaoBr = object.certification
+                
+            })
+
+            return classificacaoBr
+        }
+    }
+}
+
+// **************************************
+
+// **************************************Lançamentos
 
 async function jsonFilmesLancamentos() {
     const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&language=pt-BR&page=1`
@@ -157,7 +243,7 @@ function manipulaDadosFilmeLancamento(json) {
 
         const divFilme = document.createElement('div')
         divFilme.classList.add('imagem')
-        
+
         containerCardFilmes.appendChild(divFilme)
 
         const posterFilm = document.createElement('img')
@@ -168,12 +254,12 @@ function manipulaDadosFilmeLancamento(json) {
         divFilme.appendChild(posterFilm)
 
     }
-     
+
     const buttonVerMais = document.createElement("button")
     buttonVerMais.textContent = "VER MAIS..."
 
     containerCardFilmes.appendChild(buttonVerMais)
-    
+
 
 }
 
@@ -190,7 +276,7 @@ async function jsonFilmesAcao() {
 }
 
 async function retornaDadosFilmesAcao() {
-    
+
     const json = await jsonFilmesAcao()
 
     manipulaDadosFilmeAcao(json)
@@ -222,7 +308,7 @@ function manipulaDadosFilmeAcao(json) {
 
         const divFilme = document.createElement('div')
         divFilme.classList.add('imagem')
-        
+
         containerCardFilmes.appendChild(divFilme)
 
         const posterFilm = document.createElement('img')
@@ -253,7 +339,7 @@ async function jsonFilmesComedia() {
 }
 
 async function retornaDadosFilmesComedia() {
-    
+
     const json = await jsonFilmesComedia()
 
     manipulaDadosFilmeComedia(json)
@@ -285,7 +371,7 @@ function manipulaDadosFilmeComedia(json) {
 
         const divFilme = document.createElement('div')
         divFilme.classList.add('imagem')
-        
+
         containerCardFilmes.appendChild(divFilme)
 
         const posterFilm = document.createElement('img')
@@ -316,7 +402,7 @@ async function jsonFilmesTerror() {
 }
 
 async function retornaDadosFilmesTerror() {
-    
+
     const json = await jsonFilmesTerror()
 
     manipulaDadosFilmeTerror(json)
@@ -348,7 +434,7 @@ function manipulaDadosFilmeTerror(json) {
 
         const divFilme = document.createElement('div')
         divFilme.classList.add('imagem')
-        
+
         containerCardFilmes.appendChild(divFilme)
 
         const posterFilm = document.createElement('img')
